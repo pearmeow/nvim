34e7d1d8c50f95b7f663cc8a3d72311818b925fd
pearmeow <perryhuang2015@gmail.com>
2024-04-10 15:39:09 -0400

    vimtex

--- init.lua
+++ init.lua
@@ -852,6 +852,13 @@ require('lazy').setup({
       { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
   },
+  {
+    'lervag/vimtex',
+    lazy = false,
+    init = function()
+      -- Use init for configuration, don't use the more common "config".
+    end,
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
