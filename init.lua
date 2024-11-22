1f6475d09457d7028f1151dfe0dd20c326f71d20
pearmeow <perryhuang2015@gmail.com>
2024-11-22 14:00:44 -0500

    no lsp

--- init.lua
+++ init.lua
@@ -552,7 +552,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        basedpyright = {},
+        -- basedpyright = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
