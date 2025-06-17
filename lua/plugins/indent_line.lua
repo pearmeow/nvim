d11228acc3347958d9a19552148eddd9d988fc34
pearmeow <perryhuang2015@gmail.com>
2025-06-17 16:31:13 -0400

    Configure indent_line to not have a indent guide on the first line The spec setup property config had to be used for hooks as I know no other way of making hooks work

--- lua/plugins/indent_line.lua
+++ lua/plugins/indent_line.lua
@@ -4,10 +4,14 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {
-      scope = {
-        enabled = false,
-      },
-    },
+    config = function()
+      local hooks = require 'ibl.hooks'
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+      require('ibl').setup {
+        scope = {
+          enabled = false,
+        },
+      }
+    end,
   },
 }
