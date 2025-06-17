c8ae34d91868bc2faa90c97efe6e7275edbddd15
pearmeow <perryhuang2015@gmail.com>
2025-06-17 12:47:06 -0400

    Modularize all plugins and move them up a directory

---
+++ lua/plugins/guess-indent.lua
@@ -0,0 +1,5 @@
+-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+return {
+  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  opts = {}, -- forces plugin to be loaded so GuessIndent can be called
+}
