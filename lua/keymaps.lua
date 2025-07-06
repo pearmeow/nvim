245576dbc431b85bc7e923d269eded7e5616829c
pearmeow <perryhuang2015@gmail.com>
2025-07-06 12:04:34 -0400

    Change = to be the same as gq

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,6 +8,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Easier system clipboard with <leader>c instead of "+ prefix
 vim.keymap.set('n', '<leader>c', '"+', { desc = 'System [C]lipboard' })
 
+-- Maps the = operator to use the lsp format
+vim.keymap.set('n', '=', 'gq')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
