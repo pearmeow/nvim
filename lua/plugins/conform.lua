6f872d3dff49b2da39893d1e6cecd98f4faaae05
Perry Huang <perryhuang2015@gmail.com>
2025-07-26 10:52:58 -0400

    Add linter and formatter for js

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -30,12 +30,11 @@ return {
     -- end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      -- cpp = { 'clang-format' },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
     format_after_save = function(bufnr) -- instead of formatting on save, format after save asynchronously
       local disable_filetypes = { c = true, cpp = true }
